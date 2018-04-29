462fedd2873886bb0335100fcd21ed67cd130b60
Labo <l_abo@live.de>
2018-04-29 17:20:15 +0200

    Refactor UserSettings, use ObservableCollection, remove unused code

--- TraderForPoe/Properties/Settings.Designer.cs
+++ TraderForPoe/Properties/Settings.Designer.cs
@@ -50,11 +50,11 @@ namespace TraderForPoe.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
-        public global::System.Collections.Specialized.StringCollection QuadStash {
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?><ArrayOfString xmlns:xsi=\"http://www.w3.or" +
+            "g/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
+        public global::System.Collections.ObjectModel.ObservableCollection<System.String> QuadStash {
             get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["QuadStash"]));
+                return ((global::System.Collections.ObjectModel.ObservableCollection<System.String>)(this["QuadStash"]));
             }
             set {
                 this["QuadStash"] = value;
@@ -167,6 +167,18 @@ namespace TraderForPoe.Properties {
             }
             set {
                 this["CloseItemAfterTrade"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Einstellung {
+            get {
+                return ((string)(this["Einstellung"]));
+            }
+            set {
+                this["Einstellung"] = value;
             }
         }
     }
--- TraderForPoe/Windows/UserSettings.xaml.cs
+++ TraderForPoe/Windows/UserSettings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Windows;
 using TraderForPoe.Properties;
@@ -16,7 +17,6 @@ namespace TraderForPoe.Windows
             InitializeComponent();
         }
 
-
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true;
@@ -25,21 +25,8 @@ namespace TraderForPoe.Windows
         
         private void Click_SaveSettings(object sender, RoutedEventArgs e)
         {
-            Settings.Default.PlayerName = txt_PlayerName.Text;
-            Settings.Default.CollapsedItems = cbx_CollapseItems.IsChecked.Value;
-            Settings.Default.QuadStash = (StringCollection)lsb_QuadStash.Items.SourceCollection;
-            Settings.Default.PlayNotificationSound = cbx_PlaySound.IsChecked.Value;
-            Settings.Default.PathToClientTxt = txt_PathToClientTxt.Text;
-            Settings.Default.CustomWhisper1 = txt_CustomWhisper1.Text;
-            Settings.Default.CustomWhisper2 = txt_CustomWhisper2.Text;
-            Settings.Default.ThankYouWhisper = txt_ThankYouWhisper.Text;
-            Settings.Default.ImBusyWhisper = txt_ImBusyWhisper.Text;
-            Settings.Default.UseClipboardMonitor = cbx_UseClipBoardMonitor.IsChecked.Value;
-            Settings.Default.CloseItemAfterTrade = cbx_RemoveAfterTrade.IsChecked.Value;
-
             Settings.Default.Save();
             Settings.Default.Reload();
-
             System.Windows.Forms.MessageBox.Show("Settings saved successfully.", "Success", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
 
         }
@@ -50,26 +37,10 @@ namespace TraderForPoe.Windows
             System.Windows.Application.Current.Shutdown();
         }
 
-        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            lsb_QuadStash.Items.Refresh();
-        }
-
         private void Click_DeleteQuadStash(object sender, RoutedEventArgs e)
         {
-            IEditableCollectionView items = lsb_QuadStash.Items;
-            items.RemoveAt(lsb_QuadStash.SelectedIndex);
+            Settings.Default.QuadStash.Remove(lsb_QuadStash.SelectedItem.ToString());
         }
 
-        private void Window_GotFocus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Window_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
-        {
-            Settings.Default.Reload();
-            lsb_QuadStash.Items.Refresh();
-        }
     }
 }
