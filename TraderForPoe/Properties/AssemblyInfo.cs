dcd02ad35b17fea6e9ac1cb0fa8393bc7d0efa58
labo89 <laboo.lm@gmail.com>
2020-02-21 11:38:18 +0100

    Migrate to .NET 4.8, migrate to PackageReference, change version, add fodyweavers.xml

--- TraderForPoe/Properties/AssemblyInfo.cs
+++ TraderForPoe/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TraderForPoe")]
-[assembly: AssemblyCopyright("Labo 2018")]
+[assembly: AssemblyCopyright("Labo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
