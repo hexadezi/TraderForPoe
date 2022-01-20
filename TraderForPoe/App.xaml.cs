9f5fd8a58bb9cc9d7e03bf5994db7373d4c030c0
hexadezi <hexadezi@users.noreply.github.com>
2022-01-20 15:53:51 +0100

    ensuring that only a single instance is running

--- TraderForPoe/App.xaml.cs
+++ TraderForPoe/App.xaml.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Xml.Linq;
 using TraderForPoe.Properties;
 
 namespace TraderForPoe
 {
-    /// <summary>
-    /// Interaktionslogik für "App.xaml"
-    /// </summary>
-    public partial class App : Application
-    {
-        public App()
-        {
-            if (Settings.Default.UpgradeSettingsRequired)
-            {
-                Settings.Default.Upgrade();
-                Settings.Default.UpgradeSettingsRequired = false;
-                Settings.Default.Save();
-            }
-        }
-    }
+	/// <summary>
+	/// Interaktionslogik für "App.xaml"
+	/// </summary>
+	public partial class App : Application
+	{
+		public App()
+		{
+			// http://www.covingtoninnovations.com/mc/SingleInstance.html
+
+			Mutex mutex = new Mutex(true, "TraderForPoe", out bool createdNew);
+
+			if (!createdNew)
+			{
+				//app is already running! Exiting the application
+				Current.Shutdown();
+			}
+
+			GC.KeepAlive(mutex);
+
+			if (Settings.Default.UpgradeSettingsRequired)
+			{
+				Settings.Default.Upgrade();
+				Settings.Default.UpgradeSettingsRequired = false;
+				Settings.Default.Save();
+			}
+		}
+	}
 }
